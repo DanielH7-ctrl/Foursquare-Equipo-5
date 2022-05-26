13213dec16b769c05c69bd8bd2832282085087fa
PedroValencia07 <tvalenciatecalco@gmail.com>
2022-05-26 09:43:44 -0500

    Permiso de internet y creacion de boton

--- app/src/main/java/com/example/foursquare/MainActivity.kt
+++ app/src/main/java/com/example/foursquare/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.foursquare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 // LEO ESTUVO AQUI
 
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bloquear=findViewById<Button>(R.id.btlog)
+        bloquear.setOnClickListener{
+
+        }
     }
 }
